825cd3790fa1657bcab2842cf0dc0b6b84f629e8
bilal-webdeveloper <82528588+bilal-webdeveloper@users.noreply.github.com>
2022-02-18 22:29:30 +0100

    database reset, bug fixed, and login started

--- back/controlers/users.js
+++ back/controlers/users.js
@@ -14,17 +14,28 @@ async function createUser(req, res) {
     user
       .save()
       .then(() => res.status(201).send({ message: "utilisateur enregistré !" }))
-      .catch((err) => res.status(409).send({ message: "User pas enregistré" + err}))
-  }
+      .catch((err) => res.status(409).send({ message: "User pas enregistré" + err }))
+}
 
 function hashPassword(password) {
   const saltRounds = 10;
   return bcrypt.hash(password, saltRounds)
 }
 
-function logUser(req, res) {
+async function logUser(req, res) {
   const email = req.body.email
   const password = req.body.password
+  const user = await User.findOne({email: email})
+
+  const isPasswordOK = bcrypt.compare(password, user.password)
+  if (!isPasswordOK) {
+    res.status(403).send({ message: "Mot de passe incorrect" })
+  }
+  if (isPasswordOK) {
+    res.status(200).send({ message: "Connexion réussie" })
+  }
+  console.log("user:", user)
+  console.log("isPasswordOK:", isPasswordOK)
 }
 
 module.exports = { createUser, logUser }
--- back/mongo.js
+++ back/mongo.js
@@ -14,10 +14,11 @@ mongoose
 
 const userSchema = new mongoose.Schema({
   email: {type: String, required: true, unique: true},
-  password: {type: String, required: true}
+  password: {type: String, required: true},
 })
 userSchema.plugin(uniqueValidator)
 
 const User = mongoose.model("User", userSchema)
 
 module.exports = { mongoose, User }
+//module.exports = mongoose.model("User", userSchema)
