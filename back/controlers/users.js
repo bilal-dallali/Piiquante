ff6709743900901b858b6a8b749d36c790270143
bilal-webdeveloper <82528588+bilal-webdeveloper@users.noreply.github.com>
2022-02-19 19:28:06 +0100

    jwt

--- back/controlers/users.js
+++ back/controlers/users.js
@@ -1,41 +1,50 @@
 const { User } = require("../mongo")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const { json } = require("express")
 
 async function createUser(req, res) {
+  try {
     const { email, password } = req.body
-
     const hashedPassword = await hashPassword(password)
-
-    console.log("password:", password)
-    console.log("hashedPassword:", hashedPassword)
-
     const user = new User({ email, password: hashedPassword })
-  
-    user
-      .save()
-      .then(() => res.status(201).send({ message: "utilisateur enregistré !" }))
-      .catch((err) => res.status(409).send({ message: "User pas enregistré" + err }))
+    await user.save()
+    console.log("savedUser:", savedUser)
+
+    res.status(201).send({ message: "utilisateur enregistré !" })
+  } catch (err){
+    res.status(409).send({ message: "User pas enregistré :" + err })
+  }
 }
 
 function hashPassword(password) {
-  const saltRounds = 10;
+  const saltRounds = 10
   return bcrypt.hash(password, saltRounds)
 }
 
-async function logUser(req, res) {
-  const email = req.body.email
-  const password = req.body.password
-  const user = await User.findOne({email: email})
 
-  const isPasswordOK = await bcrypt.compare(password, user.password)
-  if (!isPasswordOK) {
-    res.status(403).send({ message: "Mot de passe incorrect" })
-  }
-  if (isPasswordOK) {
-    res.status(200).send({ message: "Connexion réussie" })
-  }
-  console.log("user:", user)
-  console.log("isPasswordOK:", isPasswordOK)
+async function logUser(req, res) {
+  try {
+    const email = req.body.email
+    const password = req.body.password
+    const user = await User.findOne({ email: email })
+
+    const isPasswordOK = await bcrypt.compare(password, user.password)
+    if (!isPasswordOK) {
+      res.status(403).send({ message: "Mot de passe incorrect" })
+    }
+    const token = createToken(email)
+    res.status(200).send({ userId: user?.id, token: token })
+    } catch (err) {
+      console.error(err)
+      res.status(500).send({ message: "Erreur interne" })
+    }
 }
 
+function createToken(email) {
+  const jwtPassword = process.env.JWT_PASSWORD
+  return jwt.sign({ email: email }, jwtPassword, { expiresIn: "24h" })
+}
+
+//User.deleteMany({}).then(() => console.log("all removed"))
 module.exports = { createUser, logUser }
